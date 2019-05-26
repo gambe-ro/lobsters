3d7cd03ce5a6fab15f73d674f45343ddb45f36f6
Simone Robutti <simone.robutti@gmail.com>
2019-05-26 10:52:43 +0200

    updated view query

--- db/views/replying_comments_v01.sql
+++ db/views/replying_comments_v01.sql
@@ -7,7 +7,9 @@ SELECT
     parent_comments.user_id as parent_comment_author_id,
     comments.user_id as comment_author_id,
     stories.user_id as story_author_id,
-    (read_ribbons.updated_at < comments.created_at) as is_unread
+    (read_ribbons.updated_at < comments.created_at) as is_unread,
+    (select votes.vote from votes where votes.user_id = read_ribbons.user_id and votes.comment_id = comments.id) as current_vote_vote,
+    (select votes.reason from votes where votes.user_id = read_ribbons.user_id and votes.comment_id = comments.id) as current_vote_reason
 FROM
     read_ribbons
 JOIN
@@ -19,9 +21,16 @@ LEFT JOIN
 WHERE
     read_ribbons.is_following = 1
     AND comments.user_id != read_ribbons.user_id
+    AND comments.is_deleted = FALSE
+    AND comments.is_moderated = FALSE
     AND
         (parent_comments.user_id = read_ribbons.user_id
          OR (parent_comments.user_id IS NULL
          AND stories.user_id = read_ribbons.user_id))
-    AND (comments.upvotes - comments.downvotes) < 0
-    AND (parent_comments.upvotes - parent_comments.downvotes) < 0;
+    AND (comments.upvotes - comments.downvotes) >= 0
+    AND (
+      parent_comments.id IS NULL
+      OR (parent_comments.upvotes - parent_comments.downvotes) >= 0
+    )
+    AND (CAST(stories.upvotes as signed) - CAST(stories.downvotes as signed)) >= 0
+;
